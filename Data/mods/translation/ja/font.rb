0b408276db318c68d050be78a124ff702cc50289
izayoi256 <qwert.izayoi@gmail.com>
2020-04-16 20:37:29 +0900

    Add ja directory

---
+++ Data/mods/translation/ja/font.rb
@@ -0,0 +1,7 @@
+# Override font names
+$override_fonts_lang = {
+    "5yearsoldfont" => ["あずきフォントP", "azukifontP"],
+}
+
+$defaultfonttype = ["あずきフォントP", "azukifontP"]
+Font.default_name = $defaultfonttype
